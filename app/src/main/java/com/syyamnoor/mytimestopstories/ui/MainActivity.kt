4cccc731518f73c77766ff0a7894bf94ee0fd6ec
syyam <syyamnoor@gmail.com>
2022-09-12 19:36:00 +0100

    Create UI, theme, and resource files

--- app/src/main/java/com/syyamnoor/mytimestopstories/ui/MainActivity.kt
+++ app/src/main/java/com/syyamnoor/mytimestopstories/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package com.syyamnoor.mytimestopstories.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.syyamnoor.mytimestopstories.R
+import com.syyamnoor.mytimestopstories.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
